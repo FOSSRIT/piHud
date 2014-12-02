6b5879d2d84fda7d361fc6fcf69dd65d2d7a0017
Brendan Whitfield <brendanw@windworksdesign.com>
2014-12-02 14:15:38 -0500

    made colors prettier

--- piHud/widgets.py
+++ piHud/widgets.py
@@ -1,21 +1,31 @@
 from PyQt4 import QtCore, QtGui, QtSvg
 import obd
 import pygal
+from pygal.style import Style
 from SVGWidget import SVGWidget
 
 class Gauge(SVGWidget):
     def __init__(self, parent, command):
         super(Gauge, self).__init__(parent)
-        super(Gauge, self).setFixedWidth(500)
+        super(Gauge, self).setFixedWidth(350)
         super(Gauge, self).setFixedHeight(400)
 
         self.command = command
+        self.style = Style(
+            background='black',
+            plot_background='transparent',
+            foreground='#53B9E8',
+            foreground_light='#53A0E8',
+            foreground_dark='transparent',
+            colors=('#53B9E8', '#53B9E8'))
 
     def get_command(self):
         return self.command
 
     def render(self, value):
-        gauge_chart = pygal.Gauge(human_readable=True, width=300, height=200)
+        gauge_chart = pygal.Gauge(human_readable=True, width=200, height=250, style=self.style)
+        
+        gauge_chart.show_legend = False
         gauge_chart.title = self.command.name
         gauge_chart.range = [0, 8000]
 
